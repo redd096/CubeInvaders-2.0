f22444f035ab6f35bae7b674c652b7edc3f19842
redd096 <martin.babino96@gmail.com>
2020-11-19 23:31:15 +0100

    radar che cambiano colore e velocità in base alla distanza del nemico

--- Assets/Scripts/Turrets/Graphics/RadarGraphics.cs
+++ Assets/Scripts/Turrets/Graphics/RadarGraphics.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [AddComponentMenu("Cube Invaders/Turret Graphics/Radar Graphics")]
 public class RadarGraphics : BuildableGraphics
 {
-    [Header("Flick")]
+    [Header("Radar")]
     [SerializeField] Transform objectToFlick = default;
-    [SerializeField] float flickSpeed = 1;
     [SerializeField] Color normalColor = Color.white;
-    [SerializeField] Color flickColor = Color.red;
+
+    [Header("Flick")]
+    [SerializeField] Gradient flickColor = default;
+    [SerializeField] float minFlick = 1;
+    [SerializeField] float maxFlick = 10;
 
     Radar radar;
 
@@ -43,27 +46,39 @@ public class RadarGraphics : BuildableGraphics
         if (objectToFlick == null) 
             return;
 
-        //sin from 0 to 1
-        float flick = Mathf.Abs(Mathf.Sin(Time.time * flickSpeed));
-
         //if enemy is attacking, flick color
         if(GetEnemy() && buildableObject.IsActive)
         {
-            SetColor(flickColor, flick);
+            SetColorFlick();
         }
         //else show normal color
         else
         {
-            SetColor(normalColor, flick);
+            SetColor(normalColor, 0);
         }
     }
 
+    void SetColorFlick()
+    {
+        //get flick speed based on enemy distance
+        float enemyDistance = Vector3.Distance(buildableObject.CellOwner.transform.position, GetEnemy().transform.position);
+        float distanceFrom0To1 = 1 - (enemyDistance / GameManager.instance.waveManager.waveConfig.distanceFromWorld);       //distance from 0 to 1
+        float flickSpeed = Mathf.Lerp(minFlick, maxFlick, distanceFrom0To1);                                                //speed from minFlick to maxFlick
+
+        //sin from 0 to 1
+        float flick = Mathf.Abs(Mathf.Sin(Time.time * flickSpeed));
+
+        //set color based on enemy distance
+        Color color = flickColor.Evaluate(distanceFrom0To1);
+
+        SetColor(color, flick);
+    }
     void SetColor(Color color, float delta)
     {
         Renderer[] renderers = objectToFlick.GetComponentsInChildren<Renderer>();
 
         //foreach renderer set color and emission
-        foreach(Renderer renderer in renderers)
+        foreach (Renderer renderer in renderers)
         {
             renderer.material.color = Color.Lerp(normalColor, color, delta);
             renderer.material.SetColor("_EmissionColor", Color.Lerp(normalColor, color, delta));
