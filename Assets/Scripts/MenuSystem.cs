b9b61d2690302c54b47bc0df015b06b7cc66cb92
redd096 <martin.babino96@gmail.com>
2021-03-03 18:29:45 +0100

    inserito menu system e salvataggi

---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using redd096;
+
+[System.Serializable]
+struct MenuStruct
+{
+    public Button button;
+    public string necessaryKey;
+}
+
+public class MenuSystem : MonoBehaviour
+{
+    [Header("Disabled Buttons")]
+    [SerializeField] bool setNotInteractable = false;
+    [SerializeField] bool changeColor = true;
+    [CanShow("changeColor")] [SerializeField] Color colorOnDisable = Color.red;
+
+    [Header("Menu")]
+    [SerializeField] MenuStruct[] levelButtons = default;
+
+    void Start()
+    {
+        foreach(MenuStruct levelButton in levelButtons)
+        {
+            //if no key, or load is succesfull
+            bool isActive = string.IsNullOrWhiteSpace(levelButton.necessaryKey) || Load(levelButton.necessaryKey);
+
+            if(isActive == false)
+            {
+                //set interactable
+                if (setNotInteractable)
+                    levelButton.button.interactable = false;
+
+                //change color
+                if (changeColor)
+                    levelButton.button.GetComponent<Image>().color = colorOnDisable;
+
+                //remove event
+                levelButton.button.onClick = new Button.ButtonClickedEvent();
+            }
+
+            //if not active, remove event
+            if (isActive == false)
+                levelButton.button.onClick = new Button.ButtonClickedEvent();
+        }
+    }
+
+    /// <summary>
+    /// Save data
+    /// </summary>
+    public static void Save(string key)
+    {
+        PlayerPrefs.SetInt(key, 1);
+    }
+
+    /// <summary>
+    /// Load data
+    /// </summary>
+    public static bool Load(string key)
+    {
+        return PlayerPrefs.GetInt(key, 0) > 0 ? true : false;
+    }
+
+    /// <summary>
+    /// Delete data
+    /// </summary>
+    public static void Delete(string key)
+    {
+        PlayerPrefs.DeleteKey(key);
+    }
+
+    /// <summary>
+    /// Delete every data
+    /// </summary>
+    public static void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
